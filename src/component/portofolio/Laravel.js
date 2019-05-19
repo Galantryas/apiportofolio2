bd62d59b1d68c0c7b773895aca23714ff65293ef
Galantryas <galantryas.2014@gmail.com>
2019-05-19 09:58:25 +0700

    first

---
+++ src/component/portofolio/Laravel.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class Laravel extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Welcome to My Site</h1>
+            </div>
+        )
+    }
+}
+
+export default Laravel;
